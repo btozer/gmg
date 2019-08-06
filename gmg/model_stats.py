225769d3af5198262a37397c6a4345bd95a7268c
btozer <btozer@ucsd.edu>
2019-08-06 10:23:21 -0700

    final layer_overhaul changes

--- gmg/model_stats.py
+++ gmg/model_stats.py
@@ -10,13 +10,18 @@ from numpy import mean, sqrt, square
 def rms(obs_x, obs_y, calc_x, calc_y):
     """CALCULATE RMS MISFIT BETWEEN OBSERVED AND CALCULATED VALUES"""
 
+    # LIMIT OBSERVED VALUES TO THE X RANGE OF THE CALCULATED VALUES
+    obs = np.column_stack((obs_x, obs_y))
+    obs = obs[obs[:, 0] >= calc_x.min()]
+    obs = obs[obs[:, 0] <= calc_x.max()]
+
     # INTERPOLATE THE VALUES OF THE CALCULATED ANOMALY AT THE OBSERVED SAMPLE POINTS
     f = interpolate.interp1d(calc_x, calc_y)  # CREATE INTERPOLATION FUNC FOR CALCULATED DATA
-    intp_calc_y = f(obs_x)  # INTERPOLATE CALC DATA ONTO OBS X POINTS
+    intp_calc_y = f(obs[:, 0])  # INTERPOLATE CALC DATA ONTO OBS X POINTS
 
     # CALCULATE THE RESIDUALS
-    res = (obs_y - intp_calc_y)
-    residuals = np.column_stack((obs_x, res))
+    res = (obs[:, 1] - intp_calc_y)
+    residuals = np.column_stack((obs[:, 0], res))
 
     # CALCULATE THE RMS VALUE, ROUNDED TO 2 D.P.
     rms_misfit = round(sqrt(mean(square(res))), 2)
